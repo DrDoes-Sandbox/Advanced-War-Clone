36756f4ad7b05ee5280726b5fdc34b5380194850
kyleslyngstad <kylesly@gmail.com>
2015-11-13 23:18:55 -0800

    Added .js and .css files
    Also simplified board to draw from a for loop.

---
+++ board_functions.js
@@ -0,0 +1,13 @@
+var layout_level1 = ["grass", "mntn_small","grass_mntn", "mntn_small", "grass_mntn","tree","tree","water_left"//Row 1
+			,"water_top_left","water_top","water_top_right","water_top_left","water_top","water_top","water_top","marsh"//Row 2
+			,"water_left","marsh","water_right","water_left","water","water","water","water" //Row 3
+			,"water_left","water","water_bottom_right","water_left","water","water","water","water" //Row 4
+			,"water_left","water_bottom_right","tree","water_bottom_left","water_bottom","water","water","water" //Row 5
+			,"water_left","water_top_right","grass","water_top_left","water_top","water","water","water" //Row 6
+			,"water_left","water","water_top","water","water","water","water_bottom","water_bottom" //Row 7
+			,"water_bottom_left","water_bottom","water_bottom","water_bottom","water_bottom","water_bottom_right","grass","grass" //Row 8
+			,"grass", "mntn_small","grass_mntn", "mntn_small", "grass_mntn","tree","tree","water_left"//Row 1
+			,"water_top_left","water_top","water_top_right","water_top_left","water_top","water_top","water_top","marsh"//Row 2
+			,"water_left","marsh","water_right","water_left","water","water","water","water" //Row 3
+			,"water_left","water","water_bottom_right","water_left","water","water","water","water" //Row 4
+			];
