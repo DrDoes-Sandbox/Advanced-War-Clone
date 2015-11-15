799f6f20b0b2036943843c05b611f551f77ef230
kyleslyngstad <kylesly@gmail.com>
2015-11-15 12:10:15 -0800

    Improved formatting again
    Switched to visual studio, the free editor appeared fine. But looking at
    the code on github introduced extra blanks spaces.

--- board_functions.js
+++ board_functions.js
@@ -27,23 +27,23 @@ var map_level1 = {
 
 //This draws a view of the map, limited to an 8x8
 function map_draw(map_data, pos_x, pos_y) {
-    	for(y = pos_y; y < (pos_y +8);y++){
-    		for(x=pos_x; x < (pos_x + 8);x++){
-    			i = map_data.width*y + x;
-    			$('#board').append('<div class="tile '+map_data.fields[i]+'"></div>');
-			}	
-			$('#board').append('<br />');
-    	}
+    for(y = pos_y; y < (pos_y +8);y++){
+        for(x=pos_x; x < (pos_x + 8);x++){
+    	    i = map_data.width*y + x;
+    		$('#board').append('<div class="tile '+map_data.fields[i]+'"></div>');
+        }	
+		$('#board').append('<br />');
+    }
 }
 			
 //This draws the entire map, used for map creation.
 function map_see(map_data) {
-		for(y = 0; y < map_data.height;y++){
- 			for(x=0; x < map_data.width;x++){
- 				i = map_data.width*y + x;
- 				$('#board').append('<div class="tile '+map_data.fields[i]+'"></div>');																 
-			}
-			$('#board').append('<br />');
- 		}
+	for(y = 0; y < map_data.height;y++){
+ 	    for(x=0; x < map_data.width;x++){
+ 		    i = map_data.width*y + x;
+            $('#board').append('<div class="tile '+map_data.fields[i]+'"></div>');																 
+        }
+        $('#board').append('<br />');
+    }
 }	 
 			
